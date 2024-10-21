0210da304cd77ea93ab4ad5319ac673e0c6b075f
Regiane <regianemelo1995@gmail.com>
2024-10-21 16:55:16 -0300

    configurando EsLint e Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-
+import { Button } from './components/ui/button'
+import './global.css'
 
 export function App() {
 
   return (
-    <h1>Hello Word</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
 
