19357306a9b5697b87edd5503dceafd4f6f4770b
Regiane <regianemelo1995@gmail.com>
2024-10-21 17:09:36 -0300

    instalando react router dom

--- routes.tsx
+++ routes.tsx
@@ -1,1 +1,10 @@
-export const router = createBro
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Dashboard } from './src/pages/auth/app/dashboard'
+import { SignIn } from './src/pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Dashboard /> },
+  { path: '/sign-in', element: <SignIn /> },
+])
