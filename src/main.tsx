0c32812afdadbab196592d5c41f4396d65d2083a
Regiane <regianemelo1995@gmail.com>
2024-10-24 16:07:02 -0300

    pagina de login

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
-import './global.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
