59d4703288e9f08b8954d40080da9b8c773c54d3
Regiane <regianemelo1995@gmail.com>
2024-11-06 11:39:12 -0300

    adicionando cartesiangrid

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,4 +1,11 @@
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import {
@@ -44,12 +51,13 @@ export function RevenueChart() {
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
             <Line
               type="linear"
               strokeWidth={2}
               dataKey="revenue"
               stroke={colors.violet['500']}
-            ></Line>
+            />
           </LineChart>
         </ResponsiveContainer>
       </CardContent>
