140d6a47a58e31f286228931a4f277bb92d2d3a2
Regiane <regianemelo1995@gmail.com>
2024-11-21 17:00:31 -0300

    paginacao dos pedidos

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,9 +1,13 @@
 import { api } from '../lib/axios'
 
+export interface GetOrdersQuery {
+  pageIndex?: number | null
+}
+
 interface GetOrdersResponse {
   orders: {
     orderId: string
-    createdAt: Date
+    createdAt: string
     status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
     customerName: string
     total: number
@@ -15,10 +19,10 @@ interface GetOrdersResponse {
   }
 }
 
-export async function getOrders() {
+export async function getOrders({ pageIndex }: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   })
 
